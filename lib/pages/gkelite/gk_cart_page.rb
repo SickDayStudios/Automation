89421cf151ad70490f07dfbd12fd55a1000d43e6
Cason Williams <cason.williams@pollinate.com>
2017-11-27 17:25:35 -0800

    omg just work already

--- lib/pages/gkelite/gk_cart_page.rb
+++ lib/pages/gkelite/gk_cart_page.rb
@@ -11,7 +11,7 @@ class GKCartPage < GKShopifyBasePage
 
 
 
-button(:secure_checkout, name: "checkout")
+button(:secure_checkout, css: "#site-container > div.container > div > div > form > div > div.col-xs-12.col-sm-4.shopping-cart__secondary > ul > li:nth-child(2) > button")
 link(:update_item, css: "#site-container > div.container > form > div > div.col-xs-12.col-sm-8.shopping-cart__primary > div.shopping-cart__item > div:nth-child(1) > div.col-xs-12.col-sm-9 > div:nth-child(4) > div.col-xs-8.col-sm-5.shopping-cart__update-remove > a:nth-child(1)")
 link(:remove_item, class: "shopping-cart__update-remove__remove")
 div(:signin_popup, id: "signin-or-guest")
