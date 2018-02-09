40557d03851d3dcfe5bd42406c0eb71a1261e4a6
Cason Williams <cason.williams@pollinate.com>
2018-02-09 14:22:12 -0800

    commented out accessibility stuff

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -28,8 +28,8 @@ RSpec.configure do |config|
 			FileUtils.mkdir_p($screenshotfolder)
 		end
 		args = ['--flag-switches-begin','--window-size=1920,1080','--disable-infobars','--flag-switches-end']
-		wave_path = '/Users/case/Library/Application Support/Google/Chrome/Profile 2/Profile 2/Extensions/jbbplnpkjmmeebjpijfedlgcdilocofh/1.0.9_0.crx'
-		lighthouse_path = '/Users/case/Library/Application Support/Google/Chrome/Profile 2/Profile 2/Extensions/blipmdconlkpinefehnmjammfjpmpbjk/2.8.0_0.crx'
+		# wave_path = '/Users/case/Library/Application Support/Google/Chrome/Profile 2/Profile 2/Extensions/jbbplnpkjmmeebjpijfedlgcdilocofh/1.0.9_0.crx'
+		# lighthouse_path = '/Users/case/Library/Application Support/Google/Chrome/Profile 2/Profile 2/Extensions/blipmdconlkpinefehnmjammfjpmpbjk/2.8.0_0.crx'
 		$driver = Watir::Browser.new :chrome , options: { args: args }
 	end
 
