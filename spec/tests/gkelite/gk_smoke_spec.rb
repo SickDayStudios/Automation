decbf8bd95265a4a0b2c0e97315a7f1c2ccd2d7a
Cason Williams <cason.williams@pollinate.com>
2018-04-10 10:15:18 -0700

    Slowing down automation for jenkins to keep up

--- spec/tests/gkelite/gk_smoke_spec.rb
+++ spec/tests/gkelite/gk_smoke_spec.rb
@@ -19,53 +19,71 @@ describe "#{ENV['SITE'].upcase}:#{ENV['ENVIRONMENT'].upcase}:#{ENV['BROWSER'].up
 	end
 
 	it ' - Navigate to Login Page' do
+		@home_page.wait_until { @home_page.password? }
+		sleep 1
 		@home_page.enter_password
+		sleep 1
 		@home_page.home_page
+		sleep 1
 		@home_page.login_page
+		sleep 1
 		expect(@login_page.url).to include('/account/login')
 	end
 
 	it ' - Sign Into Pre-Existing Account' do
 		@login_page.select_email_login_radio
+		sleep 1
 		@login_page.login_with($username, $password)
+		sleep 1
 		expect(@login_page.url).to include('/account')
 	end
 
 
 	it ' - Navigate to Product Page' do
 		@home_page.product_page('3734')
+		sleep 1
 		expect(@home_page.url).to include('3734')
 	end
 
 	it ' - Add Product to Cart' do
 		@product_page.wait_until { @product_page.product_thumbnails? }
+		sleep 1
 		@product_page.random_options
+		sleep 1
 		@product_page.add_to_cart
+		sleep 1
 		@product_page.wait_until { @product_page.cart_popup? }
+		sleep 1
 		expect(@product_page.cart_popup?).to eq(true)
 	end
 
 	it ' - Lightbox Checkout' do
 		@product_page.checkout
+		sleep 1
 		expect(@product_page.url).to include('/cart')
 	end
 
 	it ' - Secure Checkout' do
 		@cart_page.secure_checkout
+		sleep 1
 	end
 
 	it ' - Continue to Shipping Method' do
 		@checkout_page.continue_to
+		sleep 1
 	end
 
 	it ' - Continue to Payment Method' do
 		@checkout_page.continue_to
+		sleep 1
 		@checkout_page.wait_until { @checkout_page.different_address? }
+		sleep 1
 	end
 
 	if ENV['ENVIRONMENT'] == 'prod'
 		it ' - Complete Order' do
 			@checkout_page.continue_to
+			sleep 1
 			expect(@payment_page.url).to include("pay.gkelite")
 		end
 	end
@@ -74,24 +92,29 @@ describe "#{ENV['SITE'].upcase}:#{ENV['ENVIRONMENT'].upcase}:#{ENV['BROWSER'].up
 
 		it ' - Complete Order' do
 			@checkout_page.continue_to
+			sleep 1
 			expect(@payment_page.url).to include("pay-gkelite")
 		end
 
 		if ENV['USER_TYPE'] == 'dealer' || ENV['USER_TYPE'] == 'distributor' || ENV['USER_TYPE'] == 'teamlead'
 			it ' - Select Random Sales Rep' do
 				@payment_page.select_random_rep
+				sleep 1
 			end
 		end
 
 		
 		it ' - Place Order' do
 			@payment_page.place_order
+			sleep 1
 			@payment_page.wait_while { @payment_page.loader? }
+			sleep 1
 			expect(@payment_page.url).to include("processing")
 		end
 
 		it ' - Payment Processed Successfully' do
 			@payment_page.wait_while { (@payment_page.url.include? "processing") }
+			sleep 1
 			expect(@payment_page.url).to include("thank_you")
 		end
 	end
